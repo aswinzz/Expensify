86b371ad97db2d333fc44cd923d0964637f2a508
aswinzz <iit2016106@iiita.ac.in>
2018-03-28 23:31:07 +0530

    fix dist

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
-
+import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
     <div>
         <h3><Link to={`/edit/${id}`}>{description}</Link></h3>
-        <p>{amount} - {createdAt}</p>
+        <p>{amount} - {moment(createdAt).format('MMMM Do YYYY')}</p>
         <button onClick={()=>{
             dispatch(removeExpense({ id }));
         }}>Remove</button>
